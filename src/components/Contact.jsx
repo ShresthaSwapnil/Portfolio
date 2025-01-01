7e6442d2092641d41bfb16d157c63ada9e458fd6
ShresthaSwapnil <shresthaswapnil03@gmail.com>
2025-01-01 22:10:48 +0545

    Implemented Supabase for Messages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,41 @@
 import { motion } from "framer-motion";
+import supabase from "../supabase-client";
+import { useState } from "react";
 
 const Contact = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      message: event.target.message.value,
+    };
+
+    console.log(formData);
+
+    try {
+      const { data, error } = await supabase
+        .from("messages")
+        .insert([formData]);
+
+      if (error) {
+        console.error("Error inserting data:", error.message);
+        alert("Failed to send message.");
+      } else {
+        alert("Message sent successfully!");
+        event.target.reset();
+      }
+    } catch (error) {
+      console.error("Unexpected error:", error.message);
+      alert("An unexpected error occurred.");
+    }
+  };
+
   return (
     <section
       id="contact"
-      className="w-screen h-screen flex items-center justify-center p-8 bg-gray-50 dark:bg-gray-800"
+      className="w-screen h-screen flex items-center justify-center p-8 bg-gray-50 dark:bg-gray-800 max-sm:mt-64"
     >
       <div className="max-w-4xl w-full">
         <motion.h2
@@ -20,6 +51,7 @@ const Contact = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
           className="space-y-6"
+          onSubmit={handleSubmit}
         >
           <div>
             <label
@@ -32,6 +64,8 @@ const Contact = () => {
               type="text"
               id="name"
               className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
+              name="name"
+              required
             />
           </div>
           <div>
@@ -45,6 +79,8 @@ const Contact = () => {
               type="email"
               id="email"
               className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
+              name="email"
+              required
             />
           </div>
           <div>
@@ -58,6 +94,7 @@ const Contact = () => {
               id="message"
               rows={4}
               className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
+              name="message"
             ></textarea>
           </div>
           <button
