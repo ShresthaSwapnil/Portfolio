9c206110be57c9cdd8c18287ffd77a683116c753
Swapnil Shrestha <shresthaswapnil03@gmail.com>
2025-01-17 15:33:54 +0545

    Added meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { sitemapPlugin } from "vite-plugin-sitemap";
+import sitemapPlugin from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
